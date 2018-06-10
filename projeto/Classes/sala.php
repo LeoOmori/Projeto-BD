3a4099bdf05728ba330666aa55d8470b201b7403
LeoOmori <37680306+LeoOmori@users.noreply.github.com>
2018-06-10 19:52:49 -0300

    Add files via upload

---
+++ projeto/Classes/sala.php
@@ -0,0 +1,30 @@
+<?php
+
+    class sala{
+
+        public $nome;
+        public $comentario;
+        public $privado;
+        public $dono;
+
+    
+        public function criarTopico ();
+        
+        public function deletarTopico();
+
+        public function criarSala();
+
+        public function aceitarAmizade();
+
+        public function enviarConviteAmizade();
+
+        public function uparArquivo();
+
+
+    }
+
+
+
+
+
+?>
